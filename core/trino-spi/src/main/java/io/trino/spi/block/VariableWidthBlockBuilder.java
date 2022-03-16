c9556841d09680c7f2b413c1dcc1d8495847319f
Lukasz Stec <lukasz.stec@starburstdata.com>
2022-03-16 13:48:20 +0100

    Replace class isolation with manual dispatch
    To avoid block type pollution, use manual dispatch
    instead of class isolation.

--- core/trino-spi/src/main/java/io/trino/spi/block/VariableWidthBlockBuilder.java
+++ core/trino-spi/src/main/java/io/trino/spi/block/VariableWidthBlockBuilder.java
@@ -77,7 +77,7 @@ public class VariableWidthBlockBuilder
     }
 
     @Override
-    protected int getPositionOffset(int position)
+    public int getPositionOffset(int position)
     {
         checkValidPosition(position, positions);
         return getOffset(position);
@@ -91,7 +91,7 @@ public class VariableWidthBlockBuilder
     }
 
     @Override
-    protected Slice getRawSlice(int position)
+    public Slice getRawSlice(int position)
     {
         return sliceOutput.getUnderlyingSlice();
     }
