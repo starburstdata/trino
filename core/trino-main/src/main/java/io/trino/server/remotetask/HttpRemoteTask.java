5ef2106b8f0444fd5440d4fe4af90e202f1c3104
Jack Klamer <jack.klamer@starburstdata.com>
2023-11-29 18:42:09 -0600

    add Init stage to SqlTask

--- core/trino-main/src/main/java/io/trino/server/remotetask/HttpRemoteTask.java
+++ core/trino-main/src/main/java/io/trino/server/remotetask/HttpRemoteTask.java
@@ -114,6 +114,7 @@ import static io.trino.SystemSessionProperties.isRemoteTaskAdaptiveUpdateRequest
 import static io.trino.execution.DynamicFiltersCollector.INITIAL_DYNAMIC_FILTERS_VERSION;
 import static io.trino.execution.TaskInfo.createInitialTask;
 import static io.trino.execution.TaskState.FAILED;
+import static io.trino.execution.TaskState.INITIALIZING;
 import static io.trino.execution.TaskStatus.failWith;
 import static io.trino.server.remotetask.RequestErrorTracker.logError;
 import static io.trino.spi.HostAddress.fromUri;
@@ -620,6 +621,10 @@ public final class HttpRemoteTask
     private synchronized void processTaskUpdate(TaskInfo newValue, List<SplitAssignment> splitAssignments)
     {
         updateTaskInfo(newValue);
+        if (taskStatusFetcher.getTaskStatus().getState() == INITIALIZING) {
+            scheduleUpdate();
+            return;
+        }
 
         // remove acknowledged splits, which frees memory
         for (SplitAssignment assignment : splitAssignments) {
